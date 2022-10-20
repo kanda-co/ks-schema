6112b05022ea2643ce69e5c6d7a160662b057b06
Andy Hamilton <andyhmltn@gmail.com>
2022-10-20 14:13:22 +0100

    Fix: Change useLoadData key to not use random number

--- ks-frontend-services/src/hooks/useLoadData.ts
+++ ks-frontend-services/src/hooks/useLoadData.ts
@@ -3,7 +3,6 @@ import { PublicConfiguration } from 'swr/dist/types';
 
 import { handleResponse } from '../handlers';
 import { ServiceMethod } from '../types';
-import { useMemo } from 'react';
 
 const useLoadData = (
   service: ServiceMethod,
@@ -13,8 +12,7 @@ const useLoadData = (
   const method = service?.method || (() => () => {});
   const fetcher = () => method(...arg)().then((res) => handleResponse(res));
 
-  // Append a random number so that the same API endpoint can be called by different components
-  const key = useMemo(() => `${service?.key}-${Math.random()}`, [service?.key]);
+  const key = service?.key;
 
   return useSWRImmutable(key, fetcher, {
     ...options,
