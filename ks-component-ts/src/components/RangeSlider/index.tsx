47a91f3d5de53442cdde8c782e7f3f5ab93b1a00
Andy Hamilton <andyhmltn@gmail.com>
2022-11-02 19:41:58 +0000

    Feat: Finish the RangeSlider component

---
+++ ks-component-ts/src/components/RangeSlider/index.tsx
@@ -0,0 +1,47 @@
+import React, { FunctionComponent } from "react";
+import { Range } from "react-range";
+
+import type { RangeSliderProps } from "./types";
+import { CLASS_NAMES, DEFAULT_COLORS } from "./constants";
+import useRangeSliderProps from "./useRangeSliderProps";
+import Track from "~/components/RangeSlider/Track";
+import Thumb from "~/components/RangeSlider/Thumb";
+
+const RangeSlider: FunctionComponent<RangeSliderProps> = function ({
+  value,
+  defaultValue,
+  step = 1,
+  max = 100,
+  min = 0,
+  colors = DEFAULT_COLORS,
+  onChange,
+}) {
+  const { handleChange, values, trackStyle } = useRangeSliderProps({
+    value,
+    defaultValue,
+    max,
+    min,
+    colors,
+    onChange,
+  });
+
+  return (
+    <div className={CLASS_NAMES.container}>
+      <Range
+        values={values as number[]}
+        step={step}
+        min={min}
+        max={max}
+        onChange={handleChange}
+        renderTrack={({ props, children }) => (
+          <Track {...props} trackStyle={trackStyle}>
+            {children}
+          </Track>
+        )}
+        renderThumb={({ props }) => <Thumb {...props} />}
+      />
+    </div>
+  );
+};
+
+export default RangeSlider;
--- ks-component-ts/src/components/index.ts
+++ ks-component-ts/src/components/index.ts
@@ -6,6 +6,7 @@ export { default as FormState } from "./FormState";
 export { default as FormTheme } from "./FormTheme";
 export { default as MultiStepForm } from "./MultiStepForm";
 export { default as PasswordStrengthIndicator } from "./PasswordStrengthIndicator";
+export { default as RangeSlider } from "./RangeSlider";
 export { default as RowFields } from "./RowFields";
 export { default as Search } from "./Search";
 export { default as Table } from "./Table";
