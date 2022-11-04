33589eb8a8f51976a430729747532bec2e9a926d
Andy Hamilton <andyhmltn@gmail.com>
2022-11-04 16:10:13 +0000

    Feat: Add an external pdf compression service

---
+++ ks-frontend-services/src/service/external/pdf.ts
@@ -0,0 +1,43 @@
+import { RequestFunction } from '@openapi-io-ts/runtime';
+import { StringIndexedObject } from '../../types';
+import { createPoster } from './helpers';
+
+export const BASE_URL =
+  process.env.REACT_APP_PDF_COMPRESS_SERVICE_URL ||
+  'https://europe-west1-basic-garden-241310.cloudfunctions.net';
+
+const pdfPoster = <T>(
+  url: string,
+  body: StringIndexedObject = {},
+): RequestFunction<{ body: T }, T> =>
+  createPoster(BASE_URL)(url, body) as unknown as RequestFunction<
+    { body: T },
+    T
+  >;
+
+export interface CompressRequest {
+  body: {
+    content: string;
+    mimetype: string;
+  };
+}
+
+export interface FindResponse {
+  addresses: Array<string[]>;
+}
+
+export default {
+  compress: {
+    key: '/compress',
+    method: ({
+      body: { content, mimetype },
+    }: CompressRequest): RequestFunction<
+      { body: FindResponse },
+      FindResponse
+    > =>
+      pdfPoster(`/kspdf-compress`, {
+        content,
+        mimetype,
+      }),
+  },
+};
