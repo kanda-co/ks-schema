c0489d01b85bffad75ae2c56605dc6041442c142
Andy Hamilton <andyhmltn@gmail.com>
2022-11-18 19:55:18 +0000

    v0.0.137-ts-19-19

--- ks-component-ts/src/field/components/SelectionModal/index.tsx
+++ ks-component-ts/src/field/components/SelectionModal/index.tsx
@@ -1,1 +1,23 @@
-export { SelectionModalProps } from "~/field/components/SelectionModal/types";
+import withFieldInfo from "~/field/components/FieldInfo/withFieldInfo";
+import withFieldFormController from "~/field/components/FieldFormController/withFieldFormController";
+import {
+  DefaultFormFieldProps,
+  WrappedWithFieldInfoFormComponentProps,
+} from "~/field/types";
+import Uncontrolled, {
+  SelectionModalUncontrolledProps,
+} from "./SelectionModalUncontrolled";
+
+const WithFieldInfo = withFieldInfo(Uncontrolled);
+
+export { Uncontrolled, WithFieldInfo };
+
+const SelectionModal = withFieldFormController(WithFieldInfo, null, true, true);
+
+export default SelectionModal;
+
+export type SelectionModalBaseProps =
+  DefaultFormFieldProps<SelectionModalUncontrolledProps>;
+
+export type SelectionModalProps =
+  WrappedWithFieldInfoFormComponentProps<SelectionModalUncontrolledProps>;
