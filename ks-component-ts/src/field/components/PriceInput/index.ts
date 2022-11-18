9ea03fccc3b774c343b54fcc01e72e7af5089835
Andy Hamilton <andyhmltn@gmail.com>
2022-11-18 20:27:13 +0000

    Fix: AutoSizePriceInputProps

--- ks-component-ts/src/field/components/PriceInput/index.ts
+++ ks-component-ts/src/field/components/PriceInput/index.ts
@@ -1,11 +1,22 @@
-import Controlled from "./PriceInputControlled";
-import withFieldFormController from "~/field/components/FieldFormController/withFieldFormController";
+import Controlled, { PriceInputControlledProps } from "./PriceInputControlled";
+import withFieldFormController, {
+  type FieldFormControllerPropsWithoutChildren,
+} from "~/field/components/FieldFormController/withFieldFormController";
 import withFieldInfo from "../FieldInfo/withFieldInfo";
+import { type WrappedWithFieldInfoFormComponentProps } from "~/field/types";
 
 const WithFieldInfo = withFieldInfo(Controlled);
 
 export { Controlled, WithFieldInfo };
 
 const PriceInput = withFieldFormController(WithFieldInfo);
+
+export type PriceInputBaseProps = PriceInputControlledProps;
+
+export type PriceInputWithInfoProps =
+  WrappedWithFieldInfoFormComponentProps<PriceInputBaseProps>;
+
+export type PriceInputProps =
+  FieldFormControllerPropsWithoutChildren<PriceInputWithInfoProps>;
 
 export default PriceInput;
