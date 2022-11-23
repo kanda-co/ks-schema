b6e300b69a324108b7c37979d9f27a31522098ca
Andy Hamilton <andyhmltn@gmail.com>
2022-11-23 17:56:51 +0000

    Revert "v0.2.5"
    This reverts commit 50a150803c410e7c343fc7822325f4e7a4ba7f0c.

--- ks-component-ts/src/field/components/NumberFormatInput/NumberFormatInputControlled/index.tsx
+++ ks-component-ts/src/field/components/NumberFormatInput/NumberFormatInputControlled/index.tsx
@@ -1,10 +1,10 @@
 import React, { type FunctionComponent } from "react";
 import NumberFormat from "react-number-format";
 import { Controller } from "react-hook-form";
+import { Uncontrolled as InputUncontrolled } from "~/field/components/Input";
 import { type NumberFormatInputControlledProps } from "../types";
 import { defaultOnValueChange } from "./helpers";
 import { getValidationConditions } from "~/field/components/Validator/useValidationProps/helpers";
-import NumberInputUncontrolled from "~/field/components/Input/NumberInputUncontrolled";
 
 const NumberFormatInputControlled: FunctionComponent<NumberFormatInputControlledProps> =
   function ({
@@ -17,7 +17,7 @@ const NumberFormatInputControlled: FunctionComponent<NumberFormatInputControlled
     validation,
     placeholder,
     formatValue: valueFormatter = (value: string) => value,
-    customInput = NumberInputUncontrolled,
+    customInput = InputUncontrolled,
     onValueChange,
     ...restProps
   }) {
