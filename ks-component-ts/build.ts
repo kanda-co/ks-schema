81ec09d9d1ce8bd78fc968bbeacb8daf9fc04967
Andy Hamilton <andyhmltn@gmail.com>
2022-11-24 16:32:17 +0000

    v0.0.0-ts-1

---
+++ ks-component-ts/build.ts
@@ -0,0 +1,23 @@
+const { build } = require("esbuild");
+const { dependencies } = require("./package.json");
+const svgrPlugin = require("esbuild-plugin-svgr");
+
+const entryFile = "./src/index.ts";
+const shared = {
+  bundle: true,
+  entryPoints: [entryFile],
+  // Treat all dependencies in package.json as externals to keep bundle size to a minimum
+  external: Object.keys(dependencies),
+  plugins: [svgrPlugin()],
+  logLevel: "info",
+  minify: true,
+  sourcemap: true,
+};
+
+build({
+  ...shared,
+  format: "esm",
+  outfile: "./dist/index.esm.js",
+  target: ["esnext", "node12.22.0"],
+});
+
---
+++ ks-component-ts/src/serve.ts
@@ -0,0 +1,45 @@
+const { build } = require("esbuild");
+const svgrPlugin = require("esbuild-plugin-svgr");
+const chokidar = require("chokidar");
+const liveServer = require("live-server");
+
+(async () => {
+  const builder = await build({
+    bundle: true,
+    // Defines env variables for bundled JavaScript; here `process.env.NODE_ENV`
+    // is propagated with a fallback.
+    define: {
+      "process.env.NODE_ENV": JSON.stringify(
+        process.env.NODE_ENV || "development"
+      ),
+    },
+    entryPoints: ["./index.ts"],
+    plugins: [
+      svgrPlugin(),
+    ],
+    // Uses incremental compilation (see `chokidar.on`).
+    incremental: true,
+    // Removes whitespace, etc. depending on `NODE_ENV=...`.
+    minify: process.env.NODE_ENV === "production",
+    outfile: "public/script.js",
+    sourcemap: true,
+  });
+  // `chokidar` watcher source changes.
+  chokidar
+    // Watches TypeScript and React TypeScript.
+    .watch("src/**/*.{ts,tsx}", {
+      interval: 0, // No delay
+    })
+    // Rebuilds esbuild (incrementally -- see `build.incremental`).
+    .on("all", () => {
+      builder.rebuild();
+    });
+  // `liveServer` local server for hot reload.
+  liveServer.start({
+    // Opens the local server on start.
+    open: true,
+    port:8080,
+    // Uses `public` as the local server folder.
+    root: "public",
+  });
+})();
