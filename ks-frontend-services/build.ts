881faa78a1f0c4fcdfbabbc804f3977b277f9788
Andy Hamilton <andyhmltn@gmail.com>
2022-11-29 15:56:15 +0000

    Fix: Remove test code

--- ks-frontend-services/build.ts
+++ ks-frontend-services/build.ts
@@ -1,6 +1,6 @@
 const { build } = require('esbuild');
 const { nodeExternalsPlugin } = require('esbuild-node-externals');
-const copyStaticFiles = require('esbuild-copy-static-files');
+// const copyStaticFiles = require('esbuild-copy-static-files');
 const { dependencies } = require('./package.json');
 
 const entryFile = './src/index.ts';
@@ -10,10 +10,10 @@ const shared = {
   // Treat all dependencies in package.json as externals to keep bundle size to a minimum
   external: Object.keys(dependencies),
   plugins: [
-    copyStaticFiles({
-      src: './generated/dist/index.d.ts',
-      dest: './dist/index.d.ts',
-    }),
+    // copyStaticFiles({
+    //   src: './generated/dist/index.d.ts',
+    //   dest: './dist/index.d.ts',
+    // }),
     nodeExternalsPlugin(),
   ],
   logLevel: 'info',
