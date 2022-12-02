7282f0f0ab20748648b7f1126a6f1fe334caedbf
Andy Hamilton <andyhmltn@gmail.com>
2022-12-02 18:58:33 +0000

    Feat: Yalc publish on initial build

--- ks-component-ts/build.ts
+++ ks-component-ts/build.ts
@@ -5,6 +5,17 @@ const copyStaticFiles = require("esbuild-copy-static-files");
 const { nodeExternalsPlugin } = require("esbuild-node-externals");
 const { exec } = require("child_process");
 
+const handleYalcPublish = () => {
+  // @ts-ignore
+  exec("yalc publish", (e, stdout) => {
+    console.log("New version of package published to yalc");
+    // @ts-ignore
+    exec("cd ./app && yalc update", (e, stdout) => {
+      console.log("App now using new version of package from yalc");
+    });
+  });
+};
+
 const entryFile = "./src/index.ts";
 const shared = {
   bundle: true,
@@ -32,14 +43,7 @@ const shared = {
       if (error) console.error("watch build failed:", error);
       else console.log("watch build succeeded");
       // @ts-ignore
-      exec("yalc publish", (e, stdout) => {
-        console.log("New version of package published to yalc");
-        // @ts-ignore
-        exec("cd ./app && yalc update", (e, stdout) => {
-          console.log(stdout);
-          console.log("App now using new version of package from yalc");
-        });
-      });
+      handleYalcPublish();
     },
   },
 };
@@ -49,4 +53,4 @@ build({
   format: "esm",
   outfile: "./dist/index.esm.js",
   target: ["esnext", "node12.22.0"],
-});
+}).finally(handleYalcPublish);
