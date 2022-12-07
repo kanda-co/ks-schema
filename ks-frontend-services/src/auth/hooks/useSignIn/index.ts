1c11dff85173b0dd551c9136ebe5d3dc84945571
Rob Gallagher <rob@kanda.co.uk>
2022-12-07 16:24:57 +0000

    feat: reworked sign in hook

--- ks-frontend-services/src/auth/hooks/useSignIn/index.ts
+++ ks-frontend-services/src/auth/hooks/useSignIn/index.ts
@@ -1,14 +1,89 @@
+import { useState, useCallback } from 'react';
 import { RequestFunction } from '@openapi-io-ts/runtime';
-import useMutate, { MutateHook } from '../../../hooks/useMutate';
+import { track, flush } from '@amplitude/analytics-browser';
+
 import { SIGN_IN_METHODS } from './constants';
+
+import type { MutateHook } from '../../../hooks/useMutate';
+import { APP_ENV } from '../../../config';
 
 type SignInType = keyof typeof SIGN_IN_METHODS;
 
 export default function useSignIn(type: SignInType): MutateHook {
-  return useMutate(
-    SIGN_IN_METHODS[type] as unknown as RequestFunction<
-      { body: unknown },
-      unknown
-    >,
+  const [error, setError] = useState();
+  const [data, setData] = useState<Record<string, unknown>>();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const method = SIGN_IN_METHODS[type];
+
+  /**
+   * Calls the method and handles loading / error states
+   */
+  const mutate = useCallback(
+    async (...arg) => {
+      setError(null);
+      setIsSubmitting(true);
+
+      const email = arg?.[0]?.email || null;
+      const { origin, pathname } = window?.location || {};
+
+      const trackingBody = {
+        path: pathname,
+        referrer: origin,
+        info: {
+          login: {
+            type,
+            ...(email && { email }),
+          },
+        },
+      };
+
+      track('login-attempted', trackingBody);
+      flush();
+
+      try {
+        /**
+         * calls method with payload and sets current data as data
+         */
+        const res = await method(...arg);
+        setData(res);
+        /**
+         * track login success
+         */
+        track('login-succeeded', trackingBody);
+        flush();
+        /**
+         * return current data in function in case we need to add .then on mutate
+         */
+        return { data: res };
+      } catch (e) {
+        /**
+         * logs the error for debugging
+         */
+        if (APP_ENV === 'qa') console.log('Mutate error', e.errors || e);
+        /**
+         * track login failure
+         */
+        track('login-failed', trackingBody);
+        flush();
+        setError(e);
+
+        return { error: e };
+      } finally {
+        /**
+         * finally sets loading false
+         */
+        setIsSubmitting(false);
+      }
+    },
+    [type],
   );
+
+  return {
+    mutate,
+    error,
+    data,
+    isSubmitting,
+    isLoading: isSubmitting,
+  };
 }
