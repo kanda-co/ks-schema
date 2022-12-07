bd600b279c4282c6bcbf7833e15c632ba1652307
Rob Gallagher <rob@kanda.co.uk>
2022-12-07 15:32:51 +0000

    feat: added email + pass login

--- ks-frontend-services/app/src/pages/Login/index.tsx
+++ ks-frontend-services/app/src/pages/Login/index.tsx
@@ -1,16 +1,22 @@
 import { Button, Loader } from '@kanda-libs/ks-design-library';
+import { Form, Field } from '@kanda-libs/ks-component-ts';
 
 import useLogin from './useLogin';
 
 const Login = () => {
-  const { signInWithGoogle, isSubmitting } = useLogin();
+  const { signInWithGoogle, isSubmitting, form, onSubmit } = useLogin();
 
   return (
-    <div className="flex flex-col">
-      <p className="text-xl mb-6">Login</p>
-      <Button.Text label="Login" onClick={signInWithGoogle} />
-      <Loader isLoading={isSubmitting} />
-    </div>
+    <Form form={form} onSubmit={onSubmit}>
+      <div className="flex flex-col">
+        <p className="text-xl mb-6">Login</p>
+        <Field.Input name="email" label="Email" />
+        <Field.PasswordInput name="password" label="Password" />
+        <Button.Text label="Login" submit className="mb-6" />
+        <Button.Text label="Login with Google" onClick={signInWithGoogle} />
+        <Loader isLoading={isSubmitting} />
+      </div>
+    </Form>
   );
 };
 
--- ks-frontend-services/app/src/pages/Login/useLogin.tsx
+++ ks-frontend-services/app/src/pages/Login/useLogin.tsx
@@ -9,18 +9,27 @@ import {
   type UserType,
 } from '@kanda-libs/ks-frontend-services';
 import { useToast } from '@kanda-libs/ks-design-library';
-import { type StringIndexedObject } from '@kanda-libs/ks-component-ts';
+import { useForm, type StringIndexedObject } from '@kanda-libs/ks-component-ts';
 
 import { getError } from './helpers';
 import { URLS } from '../../config';
+
+interface LoginFormProps {
+  email: string;
+  password: string;
+}
 
 export default function useApplyForFinance() {
   const { mutate: googleSignIn, isLoading: googleIsSubmitting } =
     useSignIn('googlePopup');
 
+  const { mutate: login, isLoading: loginIsSubmitting } = useSignIn('email');
+
   const { showError } = useToast();
 
   const navigate = useNavigate();
+
+  const form = useForm();
 
   const { revalidate, isValidating } = useCurrentUser();
 
@@ -31,8 +40,6 @@ export default function useApplyForFinance() {
       StringIndexedObject
     >,
   );
-
-  // revalidate().then(() => me({}).then(() => navigate))
 
   const afterSignIn = useCallback(async () => {
     await revalidate();
@@ -50,9 +57,22 @@ export default function useApplyForFinance() {
     });
   }, [googleSignIn, afterSignIn, showError]);
 
-  const isSubmitting = googleIsSubmitting || meIsSubmitting || isValidating;
+  const onSubmit = useCallback((formValues: LoginFormProps): void => {
+    login(formValues, false).then(({ error: loginError }) => {
+      if (loginError) {
+        showError(getError(loginError));
+        return;
+      }
+      afterSignIn();
+    });
+  }, []);
+
+  const isSubmitting =
+    googleIsSubmitting || meIsSubmitting || isValidating || loginIsSubmitting;
 
   return {
+    form,
+    onSubmit,
     signInWithGoogle,
     isSubmitting,
   };
