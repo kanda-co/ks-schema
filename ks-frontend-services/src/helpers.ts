22faadfba452b1be92766174232132615cc23de1
Andy Hamilton <andyhmltn@gmail.com>
2023-01-03 17:49:57 +0000

    Fix: Move useLoadServerData to a helper instead as it's not really a hook

--- ks-frontend-services/src/helpers.ts
+++ ks-frontend-services/src/helpers.ts
@@ -1,6 +1,11 @@
 import * as operations from './generated/operations';
-import { StringIndexedObject } from './types';
+import type {
+  ServiceMethod,
+  StringIndexedObject,
+  ServiceParams,
+} from './types';
 import fetch, { originalFetch } from './fetch';
+import { handleResponse, type Response as ServiceResponse } from 'handlers';
 
 /**
  * Call fetch, including the baseUrl and attaching headers including authentication
@@ -20,3 +25,23 @@ export const fetchRequestAdapter = (baseURL: string, requireAuth = true) => {
  */
 export const requestFunctions = (baseURL: string) =>
   operations.requestFunctionsBuilder(fetchRequestAdapter(baseURL));
+
+/**
+ * Helper function used to request data from a service whilst running on the server / node
+ */
+export const loadServerData = async <
+  Value,
+  Params = undefined,
+  Body = undefined,
+>(
+  serviceMethod: ServiceMethod<Value, Params, Body>,
+  args: ServiceParams<Params, Body>[],
+) => {
+  const method = serviceMethod || (() => () => {});
+  const fetcher = () =>
+    (method(...args)() as unknown as Promise<Response>).then((res) =>
+      handleResponse(res as unknown as any),
+    );
+
+  return fetcher();
+};
--- ks-frontend-services/src/hooks/useLoadServerData.d.ts
+++
@@ -1,3 +0,0 @@
-import { ServiceMethod } from '../types';
-declare const useLoadServerData: <Value, Params = undefined, Body_1 = undefined>(serviceMethod: ServiceMethod<Value, Params, Body_1>, ...arg: any[]) => Promise<unknown>;
-export default useLoadServerData;
