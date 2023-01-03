b227b488cf288ca52e8a9002ddb84c6632ecdad9
Andy Hamilton <andyhmltn@gmail.com>
2023-01-03 13:02:26 +0000

    Fix: Add building_number to the address format method

--- ks-component-ts/src/field/components/CompanyLookupInput/helpers.ts
+++ ks-component-ts/src/field/components/CompanyLookupInput/helpers.ts
@@ -1,10 +1,10 @@
 import type { Address } from "@kanda-libs/ks-frontend-services";
 
 export const formatAddress = ({
-  building_name,
+  building_number,
   line_1,
   line_2,
   city,
   postcode,
 }: Partial<Address>): string =>
-  [building_name, line_1, line_2, city, postcode].filter(Boolean).join(", ");
+  [building_number, line_1, line_2, city, postcode].filter(Boolean).join(", ");
