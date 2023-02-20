586a796be1b858ba2b673fe6f3d144bdac9120ff
Andy Hamilton <andyhmltn@gmail.com>
2023-02-20 15:05:42 +0000

    Refactor: Remove wonky logic around following the cursor

--- ks-component-ts/src/field/components/RichTextInput/RichTextInputControlled/RichTextInputMenu/index.tsx
+++ ks-component-ts/src/field/components/RichTextInput/RichTextInputControlled/RichTextInputMenu/index.tsx
@@ -1,21 +1,9 @@
 import React, { type FunctionComponent } from "react";
 import RichTextInputStyleButton from "../RichTextInputStyleButton";
-import { getMenuStyle } from "./helpers";
-import useMenuSelectionPosition from "./useMenuSelectionPosition";
 
 const RichTextInputMenu: FunctionComponent = function () {
-  const { hideMenu, ...selectionPosition } = useMenuSelectionPosition();
-
-  // Don't show the menu if the selection is at the beginning of the editor
-  if (hideMenu) {
-    return <></>;
-  }
-
   return (
-    <div
-      className="flex flex-row mb-2 border border-solid gap-x-2 border-neutral-200"
-      style={getMenuStyle(selectionPosition)}
-    >
+    <div className="flex flex-row mb-2 border border-solid gap-x-2 border-neutral-200">
       <RichTextInputStyleButton label="B" editorStyle="bold" />
       <RichTextInputStyleButton label="I" editorStyle="italic" />
       <RichTextInputStyleButton
