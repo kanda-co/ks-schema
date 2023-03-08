d0acf601852073342c95e8aa2977af67de123073
Andy Hamilton <andyhmltn@gmail.com>
2023-03-08 18:39:28 +0000

    Fix: Add a background to the RichTextInputMenu

--- ks-component-ts/src/field/components/RichTextInput/RichTextInputControlled/RichTextInputMenu/constants.ts
+++ ks-component-ts/src/field/components/RichTextInput/RichTextInputControlled/RichTextInputMenu/constants.ts
@@ -1,4 +1,4 @@
 export const CLASS_NAMES = {
   wrapper:
-    "absolute right-0 flex flex-row mb-2 -mt-10 md:-mt-8 border border-solid border-neutral-500 text-neutral-500 rounded-md shadow-card overflow-hidden",
+    "absolute right-0 flex flex-row mb-2 -mt-10 md:-mt-8 border border-solid border-neutral-500 text-neutral-500 rounded-md shadow-card overflow-hidden bg-neutral-000",
 };
