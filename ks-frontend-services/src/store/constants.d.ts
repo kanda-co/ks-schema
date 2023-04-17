2e26b1687a6df5e6b1186f1c0fbe35cfc76f707d
Andy Hamilton <andyhmltn@gmail.com>
2023-04-17 11:01:35 +0100

    Feat: Styling changes

--- ks-frontend-services/src/store/constants.d.ts
+++ ks-frontend-services/src/store/constants.d.ts
@@ -1,13 +1,13 @@
 export declare const NORMALIZED_INITIAL_STATE: {
-    data: any[];
-    allIds: any[];
+  data: any[];
+  allIds: any[];
 };
 export declare const GENERATED_INITIAL_STATE: {
-    id: any;
-    fetchedList: boolean;
-    isLoading: boolean;
-    isSubmitting: boolean;
-    data: any[];
-    allIds: any[];
+  id: any;
+  fetchedList: boolean;
+  isLoading: boolean;
+  isSubmitting: boolean;
+  data: any[];
+  allIds: any[];
 };
 export declare const IGNORED_ACTIONS: string[];
--- ks-frontend-services/src/store/selectors/app.d.ts
+++ ks-frontend-services/src/store/selectors/app.d.ts
@@ -1,4 +1,5 @@
 export declare const getApp: (state: any) => any;
-export declare const getPathKey: ((state: any) => any) & import("reselect").OutputSelectorFields<(args_0: any) => any> & {
+export declare const getPathKey: ((state: any) => any) &
+  import('reselect').OutputSelectorFields<(args_0: any) => any> & {
     clearCache: () => void;
-};
+  };
