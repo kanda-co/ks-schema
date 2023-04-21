57ea081be30da76f75820511e9fe5aba5311cf46
Andy Hamilton <andyhmltn@gmail.com>
2023-04-21 17:26:31 +0100

    Fix: Dispatch loading action for infoEntity calls

--- ks-frontend-services/src/hooks/useLoadData/index.d.ts
+++ ks-frontend-services/src/hooks/useLoadData/index.d.ts
@@ -1,4 +1,4 @@
 import type { LoadDataHookOptions } from './types';
 import type { Service } from '../../types';
-declare const useLoadData: <Value, Params = undefined, Body_1 = undefined>(service?: false | Service<Value, Params, Body_1>, options?: LoadDataHookOptions, ...arg: any[]) => import("swr").SWRResponse<Value, any>;
+declare const useLoadData: <Value, Params = undefined, Body_1 = undefined>(service?: false | Service<Value, Params, Body_1>, options?: Partial<LoadDataHookOptions>, ...arg: any[]) => import("swr").SWRResponse<Value, any>;
 export default useLoadData;
--- ks-frontend-services/src/service/external/helpers.d.ts
+++ ks-frontend-services/src/service/external/helpers.d.ts
@@ -1,4 +1,4 @@
 import { TaskEither } from 'fp-ts/lib/TaskEither';
 import type { StringIndexedObject } from '../../types';
 export declare const createGetter: (baseUrl: string) => (url: string) => TaskEither<Error, Response>;
-export declare const createPoster: (baseUrl: string) => (url: string, body?: StringIndexedObject) => TaskEither<Error, Response>;
+export declare const createPoster: (baseUrl: string, auth?: boolean) => (url: string, body?: StringIndexedObject) => TaskEither<Error, Response>;
