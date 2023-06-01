2be0f30cd2915641f19b006c7b920ef9aa065f62
Andy Hamilton <andyhmltn@gmail.com>
2023-06-01 17:29:31 +0100

    Feat: Updated types

--- ks-frontend-services/src/store/index.d.ts
+++ ks-frontend-services/src/store/index.d.ts
@@ -287,21 +287,9 @@ export declare function createSelectors<State extends {
         query: import("./slices/query").QueryState;
     }) => import("./slices/query").QueryFlags;
     getAuth: (state: State) => AuthState;
-    getUser: ((state: State) => import("..").AuthUser) & import("reselect").OutputSelectorFields<(args_0: AuthState) => import("..").AuthUser & {
-        clearCache: () => void;
-    }> & {
-        clearCache: () => void;
-    };
-    getAuthIsLoading: ((state: State) => boolean) & import("reselect").OutputSelectorFields<(args_0: AuthState) => boolean & {
-        clearCache: () => void;
-    }> & {
-        clearCache: () => void;
-    };
-    getIsUserLoggedIn: ((state: State) => boolean) & import("reselect").OutputSelectorFields<(args_0: import("..").AuthUser, args_1: boolean) => boolean & {
-        clearCache: () => void;
-    }> & {
-        clearCache: () => void;
-    };
+    getUser: (state: State) => import("..").AuthUser;
+    getAuthIsLoading: (state: State) => boolean;
+    getIsUserLoggedIn: (state: State) => boolean;
     getRoot: (state: State) => State;
     getApp: (state: State) => State["app"];
     getPathKey: (state: State) => import("./types").PathKey<Pages>;
--- ks-frontend-services/src/store/selectors/auth.d.ts
+++ ks-frontend-services/src/store/selectors/auth.d.ts
@@ -1,21 +1,12 @@
 import type { AuthState } from '../slices/auth';
+export interface AuthSelectors<State extends {
+    auth: AuthState;
+}> {
+    getAuth: (state: State) => AuthState;
+    getUser: (state: State) => AuthState['user'];
+    getAuthIsLoading: (state: State) => AuthState['isLoading'];
+    getIsUserLoggedIn: (state: State) => boolean;
+}
 export declare const getAuthSelectors: <State extends {
     auth: AuthState;
-}>() => {
-    getAuth: (state: State) => AuthState;
-    getUser: ((state: State) => import("../..").AuthUser) & import("reselect").OutputSelectorFields<(args_0: AuthState) => import("../..").AuthUser & {
-        clearCache: () => void;
-    }> & {
-        clearCache: () => void;
-    };
-    getAuthIsLoading: ((state: State) => boolean) & import("reselect").OutputSelectorFields<(args_0: AuthState) => boolean & {
-        clearCache: () => void;
-    }> & {
-        clearCache: () => void;
-    };
-    getIsUserLoggedIn: ((state: State) => boolean) & import("reselect").OutputSelectorFields<(args_0: import("../..").AuthUser, args_1: boolean) => boolean & {
-        clearCache: () => void;
-    }> & {
-        clearCache: () => void;
-    };
-};
+}>() => AuthSelectors<State>;
