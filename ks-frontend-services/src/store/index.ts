8aebe5e18372e4a1c5c2b37facc601fc1cca99ee
Andy Hamilton <andyhmltn@gmail.com>
2023-06-02 16:55:35 +0100

    Feat: Type casting in createStore

--- ks-frontend-services/src/store/index.ts
+++ ks-frontend-services/src/store/index.ts
@@ -3,6 +3,7 @@ import { slices as allSlices, getSelectors } from '..';
 import { createAppSlice } from './slices/app';
 import auth from './slices/auth';
 import { getAppSelectors } from './selectors/helpers';
+import { ToolkitStore } from '@reduxjs/toolkit/dist/configureStore';
 
 // Helpers types, so we can correctly infer the state of the store
 // even when passing in extra reducers
@@ -12,8 +13,8 @@ type ReducerMap<M> = {
   [K in keyof M]: Reducer<M[K]>;
 };
 
-export function createStore<PageKeys extends string, M>(
-  extraReducers: ReducerMap<M>,
+export function createStore<PageKeys extends string, ExtraState>(
+  extraReducers: ReducerMap<ExtraState>,
 ) {
   const appSlice = createAppSlice<PageKeys>();
   const app = appSlice.reducer;
@@ -28,7 +29,12 @@ export function createStore<PageKeys extends string, M>(
       }),
   });
 
-  return store;
+  type RootState = ReturnType<typeof store.getState> & ExtraState;
+
+  // It's not nice having to cast like this, but the types do not infer
+  // correctly otherwise because the types for this repo are bundled as
+  // part of the library
+  return store as unknown as ToolkitStore<RootState>;
 }
 
 export function createSelectors<State, Pages>() {
