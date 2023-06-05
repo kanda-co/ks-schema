b57b584f77fa7279ebf219b921a05000976e0e28
Andy Hamilton <andyhmltn@gmail.com>
2023-06-05 09:12:57 +0100

    Feat/checkout (#78)
    * Feat: Add auth selectors
    
    * Feat: better typing for auth selectors
    
    * Feat: Updated types
    
    * Feat: Started working on action specific reducers
    
    * Feat: Added single action reducers for cases such as checkJob & jobCompanyInfo
    
    * Fix: selectors from last commit
    
    * Fix: Managed to fix the slices index compilation errors
    
    * Feat: Started adding support for CheckJob
    
    * Feat: Change the code gen to allow for entity name override, so for instance the checkJob action, refers to JobCreditState, not CheckJob
    
    * Feat: Remove test code
    
    * Merge main

---
+++ ks-frontend-services/src/store/selectors/auth.d.ts
@@ -0,0 +1,12 @@
+import type { AuthState } from '../slices/auth';
+export interface AuthSelectors<State extends {
+    auth: AuthState;
+}> {
+    getAuth: (state: State) => AuthState;
+    getUser: (state: State) => AuthState['user'];
+    getAuthIsLoading: (state: State) => AuthState['isLoading'];
+    getIsUserLoggedIn: (state: State) => boolean;
+}
+export declare const getAuthSelectors: <State extends {
+    auth: AuthState;
+}>() => AuthSelectors<State>;
