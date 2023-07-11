52597bcfc0f5b71256e40482227f52f522351f90
Andy Hamilton <andyhmltn@gmail.com>
2023-07-11 10:07:29 +0100

    Fix: Move amplitude to peer dependency and use amplitude object for logging in fetch (#98)

--- ks-frontend-services/src/middleware/index.d.ts
+++ ks-frontend-services/src/middleware/index.d.ts
@@ -4,6 +4,7 @@ import type { GuardFunctionRouteProps, GuardToRoute, Next } from 'react-router-g
 import type { ToolkitStore } from '@reduxjs/toolkit/dist/configureStore';
 import type { PathKey } from '../store/types';
 import type { PageList } from './types';
+import { WrapperProps } from './Page';
 import { CreatePageArgs } from './helpers';
 import type { StringIndexedObject } from '../types';
 import type { RoutedApp } from './types';
@@ -19,5 +20,5 @@ export declare function fetchPageInitialData<State, P extends StringIndexedObjec
 export declare function initialDataProvider<State, P extends StringIndexedObject>(store: ToolkitStore<State>, pages: PageList<P>, pathKey: PathKey<P>): PathKey<P>;
 export declare function isAuthed<P extends StringIndexedObject>(pathKey: PathKey<P>, store: ToolkitStore): () => Promise<TE.TaskEither<Error, PathKey<P>>>;
 export declare function createMiddleware<State, P extends StringIndexedObject>(store: ToolkitStore<State>, pages: PageList<P>): (to: GuardToRoute, from: GuardFunctionRouteProps | null, next: Next) => void;
-export declare function createRoutedApp<State, Keys extends string | number, ExtraState = {}>(store: ToolkitStore<State & ExtraState>, args: Record<Keys, CreatePageArgs<State>>, notFoundPage?: FunctionComponent, Wrapper?: FunctionComponent): RoutedApp<Keys>;
+export declare function createRoutedApp<State, Keys extends string | number, ExtraState = {}>(store: ToolkitStore<State & ExtraState>, args: Record<Keys, CreatePageArgs<State>>, notFoundPage?: FunctionComponent, Wrapper?: FunctionComponent<WrapperProps>): RoutedApp<Keys>;
 export {};
