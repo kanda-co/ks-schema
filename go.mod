039d3eea869dcb8c97a198d734963ddfd36225d0
Anzel Lai <anzel_lai@hotmail.com>
2023-08-21 12:44:44 +0100

    wip: journey comms v2 pre-requisites to render template from context

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/kanda-co/ks-schema
 go 1.16
 
 require (
+	github.com/aymerick/raymond v2.0.2+incompatible // indirect
 	github.com/deepmap/oapi-codegen v1.11.0
 	github.com/getkin/kin-openapi v0.100.0
 	github.com/labstack/echo/v4 v4.9.0
+	github.com/mailgun/raymond v2.0.2+incompatible // indirect
 )
---
+++ pkg/schema/schema.comms.go
@@ -0,0 +1,73 @@
+package schema
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/mailgun/raymond"
+)
+
+var (
+	defaultTradingName  = "Kanda"
+	defaultContactName  = "Kanda"
+	defaultContactPhone = ""
+	defaultContactEmail = Email("help@kanda.co.uk")
+)
+
+// RenderTemplate with given comm context
+func RenderTemplate(cc CommContext) (CommContext, error) {
+	if cc.Sender.ContactName == nil {
+		cc.Sender.ContactName = &defaultContactName
+	}
+	if cc.Sender.ContactEmail == nil {
+		cc.Sender.ContactEmail = &defaultContactEmail
+	}
+	if cc.Sender.ContactPhone == nil {
+		cc.Sender.ContactPhone = &defaultContactPhone
+	}
+	if cc.Sender.TradingName == nil {
+		cc.Sender.TradingName = &defaultTradingName
+	}
+
+	if cc.Receiver.ContactName == nil {
+		cc.Receiver.ContactName = &defaultContactName
+	}
+	if cc.Receiver.ContactEmail == nil {
+		cc.Receiver.ContactEmail = &defaultContactEmail
+	}
+	if cc.Receiver.ContactPhone == nil {
+		cc.Receiver.ContactPhone = &defaultContactPhone
+	}
+	if cc.Receiver.TradingName == nil {
+		cc.Receiver.TradingName = &defaultTradingName
+	}
+
+	result, err := raymond.Render(cc.Template, cc)
+	if err != nil {
+		return cc, fmt.Errorf("render comm template error - %v", err)
+	}
+	cc.Template = result
+	return cc, nil
+}
+
+// ToMap turn comm context to a map data
+func (cc CommContext) ToMap() (map[string]interface{}, error) {
+	m := map[string]interface{}{}
+	b, err := json.Marshal(cc)
+	if err != nil {
+		return m, fmt.Errorf("invalid comm context - %v", err)
+	}
+	if err = json.Unmarshal(b, &m); err != nil {
+		return m, fmt.Errorf("invalid comm context data - %v", err)
+	}
+	return m, nil
+}
+
+// ToJSON turn comm context to a JSON bytes
+func (cc CommContext) ToJSON() ([]byte, error) {
+	b, err := json.Marshal(cc)
+	if err != nil {
+		return nil, fmt.Errorf("invalid comm context - %v", err)
+	}
+	return b, nil
+}
