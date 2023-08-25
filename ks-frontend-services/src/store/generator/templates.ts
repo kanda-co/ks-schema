34f604af51c31fb4e7323e14f07edb8ff927f1e8
Andy Hamilton <andyhmltn@gmail.com>
2023-08-25 14:33:52 +0100

    Fix: Change the isLoading to only set to true on get actions

--- ks-frontend-services/src/store/generator/templates.ts
+++ ks-frontend-services/src/store/generator/templates.ts
@@ -23,7 +23,8 @@ const reducerForAction = (entityName: string, actionName: string) => {
   return `
 builder.addCase(${actionName}.pending, (state) => ({
 	...state,
-	isSubmitting: true,
+	isLoading: ${actionName.includes('get') ? 'true' : 'state.isLoading'},
+	isSubmitting: ${!actionName.includes('get') ? 'true' : 'state.isSubmitting'},
 }));
 builder.addCase(${actionName}.fulfilled, ${fulfilledAction});
 builder.addCase(${actionName}.rejected, (state, action) => {
