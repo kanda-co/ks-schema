a3cb5f93dd5c68f25dd5f9a06d02f56903b49ab7
Anzel Lai <anzel_lai@hotmail.com>
2023-08-31 11:27:57 +0100

    feat: add company referrals endpoint + add From cid on referral

--- ks-frontend-services/src/store/constants.ts
+++ ks-frontend-services/src/store/constants.ts
@@ -21,6 +21,7 @@ export const IGNORED_ACTIONS = [
   'infoSession',
   'postCompanyBilling',
   'postCompanyDirectorVerification',
+  'postCompanyReferrals',
   'getCompanyDirectorVerification',
 ];
 
