b640860cd6a56a9fe9d6aeed2c09cd34471d1bb6
Rob Gallagher <rob@kanda.co.uk>
2024-01-26 20:02:16 +0000

    feat: added ev stripe promise

--- ks-frontend-services/src/lib/stripe.ts
+++ ks-frontend-services/src/lib/stripe.ts
@@ -1,5 +1,7 @@
 import { loadStripe } from '@stripe/stripe-js';
 
-import { PAYMENT_PUBLISHABLE_KEY } from '../config';
+import { EV_PAYMENT_PUBLISHABLE_KEY, PAYMENT_PUBLISHABLE_KEY } from '../config';
 
 export const stripePromise = loadStripe(PAYMENT_PUBLISHABLE_KEY);
+
+export const evStripePromise = loadStripe(EV_PAYMENT_PUBLISHABLE_KEY);
