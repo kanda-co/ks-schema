64247251cb9d6209f51870b21131f49fdfa881f7
Rob Gallagher <rob@kanda.co.uk>
2024-01-30 10:10:19 +0000

    feat: updated naming to shop

--- ks-frontend-services/src/lib/stripe.ts
+++ ks-frontend-services/src/lib/stripe.ts
@@ -1,7 +1,10 @@
 import { loadStripe } from '@stripe/stripe-js';
 
-import { EV_PAYMENT_PUBLISHABLE_KEY, PAYMENT_PUBLISHABLE_KEY } from '../config';
+import {
+  SHOP_PAYMENT_PUBLISHABLE_KEY,
+  PAYMENT_PUBLISHABLE_KEY,
+} from '../config';
 
 export const stripePromise = loadStripe(PAYMENT_PUBLISHABLE_KEY);
 
-export const evStripePromise = loadStripe(EV_PAYMENT_PUBLISHABLE_KEY);
+export const shopStripePromise = loadStripe(SHOP_PAYMENT_PUBLISHABLE_KEY);
