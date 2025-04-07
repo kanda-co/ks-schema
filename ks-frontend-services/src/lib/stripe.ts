36d544a3564945d008f48499109b52b4ff9a4823
Rob Gallagher <rob@kanda.co.uk>
2025-04-07 11:23:40 +0100

    feat: added bacs stripe

--- ks-frontend-services/src/lib/stripe.ts
+++ ks-frontend-services/src/lib/stripe.ts
@@ -3,8 +3,9 @@ import { loadStripe } from '@stripe/stripe-js';
 import {
   SHOP_PAYMENT_PUBLISHABLE_KEY,
   PAYMENT_PUBLISHABLE_KEY,
+  BACS_PAYMENT_PUBLISHABLE_KEY,
 } from '../config';
 
 export const stripePromise = loadStripe(PAYMENT_PUBLISHABLE_KEY);
-
 export const shopStripePromise = loadStripe(SHOP_PAYMENT_PUBLISHABLE_KEY);
+export const bacsStripePromise = loadStripe(BACS_PAYMENT_PUBLISHABLE_KEY);
